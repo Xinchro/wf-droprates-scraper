82fca26e7d065c4b7b583d20a7abc903824af675
Xinchro <TheXrocks@gmail.com>
2017-08-01 20:41:24 +0100

    added scss command to npm, added sass-cache to gitignore, added vue and did some basic functionality with it and added basic, initial, styling

--- src/client/script.js
+++ src/client/script.js
@@ -1,15 +1,47 @@
-function getData(name) {
-  var xmlHttp = new XMLHttpRequest()
-  xmlHttp.onreadystatechange = function() { 
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-          updateData(xmlHttp.responseText)
+let app = new Vue({
+  el: '#vue-wrapper',
+  data: {
+    message: "No data selected!",
+    filters: [
+      "glossary",
+      "missionRewards",
+      "relicRewards",
+      "keyRewards",
+      "transientRewards",
+      "sortiesRewards",
+      "modsByMod",
+      "modsByEnemy",
+      "blueprintsByBlueprint",
+      "blueprintsByEnemy"
+    ],
+    menuVisible: false
+  },
+  methods: {
+    getData(name) {
+      var xmlHttp = new XMLHttpRequest()
+      xmlHttp.onreadystatechange = () => { 
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            this.updateData(xmlHttp.responseText)
+      }
+      xmlHttp.open("GET", `http://localhost:8080/${name}.json`, true) // true for asynchronous 
+      xmlHttp.send(null)
+    },
+
+    updateData(data) {
+      document.getElementById("wrap").innerHTML = data
+      this.message = data
+    },
+
+    toggleMenu() {
+      console.log("toggling")
+      this.menuVisible = !this.menuVisible
+    }
   }
-  xmlHttp.open("GET", `http://localhost:8080/${name}.json`, true) // true for asynchronous 
-  xmlHttp.send(null)
+})
 
-  // console.log()
-}
-
-function updateData(data) {
-  document.getElementById("wrap").innerHTML = data
-}
+// let app = new Vue({
+//   el: '#wrap',
+//   data: {
+//     message: data
+//   }
+// })
