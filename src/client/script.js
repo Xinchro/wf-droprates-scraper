65c83b7456daeb7010b05b45fc941c1b725b6967
Xinchro <TheXrocks@gmail.com>
2017-08-02 16:02:15 +0100

    added basic data fetching and displaying and basic styling to lay it out

--- src/client/script.js
+++ src/client/script.js
@@ -3,45 +3,93 @@ let app = new Vue({
   data: {
     message: "No data selected!",
     filters: [
-      "glossary",
-      "missionRewards",
-      "relicRewards",
-      "keyRewards",
-      "transientRewards",
-      "sortiesRewards",
-      "modsByMod",
-      "modsByEnemy",
-      "blueprintsByBlueprint",
-      "blueprintsByEnemy"
+      // { name: "glossary",
+      //   id: 0 },
+      { name: "missionRewards",
+        label: "mission rewards",
+        id: 1 },
+      { name: "relicRewards",
+        label: "relic rewards",
+        id: 2 },
+      { name: "keyRewards",
+        label: "key rewards",
+        id: 3 },
+      { name: "transientRewards",
+        label: "transient rewards",
+        id: 4 },
+      { name: "sortiesRewards",
+        label: "sorties rewards",
+        id: 5 },
+      { name: "modsByMod",
+        label: "mods by mod",
+        id: 6 },
+      { name: "modsByEnemy",
+        label: "mods by enemy",
+        id: 7 },
+      { name: "blueprintsByBlueprint",
+        label: "blueprints by blueprint",
+        id: 8 },
+      { name: "blueprintsByEnemy",
+        label: "blueprints by enemy",
+        id: 9 }
     ],
-    menuVisible: false
+    menuVisible: true,
+    currentFilters: [],
+    dropdata: {}
   },
   methods: {
     getData(name) {
       var xmlHttp = new XMLHttpRequest()
       xmlHttp.onreadystatechange = () => { 
           if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            this.updateData(xmlHttp.responseText)
+            this.dropdata = JSON.parse(xmlHttp.responseText)
       }
       xmlHttp.open("GET", `http://localhost:8080/${name}.json`, true) // true for asynchronous 
       xmlHttp.send(null)
     },
 
     updateData(data) {
-      document.getElementById("wrap").innerHTML = data
+      // document.getElementById("wrap").innerHTML = data
       this.message = data
     },
 
     toggleMenu() {
-      console.log("toggling")
       this.menuVisible = !this.menuVisible
+    },
+
+    checkbox(id) {
+      return document.getElementById(id)
+    },
+
+    updateCurrentFilters() {
+      this.currentFilters = []
+
+      for(let i=0;i<this.filters.length;i++) {
+        if(this.checkbox(i+1).checked) {
+          this.currentFilters.push(i+1)
+        }
+      }
+
+      this.updateSearchResults()
+    },
+
+    updateSearchResults() {
+      let tempMsg = ""
+      let data = {}
+      for(let i=0;i<this.filters.length;i++) {
+        for(let j=0;j<this.currentFilters.length;j++) {
+          if(this.currentFilters[j] === this.filters[i].id) {
+            tempMsg += `${this.filters[i].label} `
+            // TODO do this properly
+            this.getData(this.filters[i].name)
+          }
+        }
+      }
+
+      if(tempMsg === "") tempMsg = "No data selected!"
+      this.message = tempMsg
+
+      // this.dropdata = data
     }
   }
 })
-
-// let app = new Vue({
-//   el: '#wrap',
-//   data: {
-//     message: data
-//   }
-// })
